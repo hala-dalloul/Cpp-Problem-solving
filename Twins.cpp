43ae7cb65ea0cdd79f9e8e860db20ddd83cf39fc
Hala M. Dalloul <haladalloul42@gmail.com>
2026-08-23 18:37:28 +0300

    WIP: scaffold solution for array-sum-based problem
    - Reads t integers into vector v
    - Sorts v in ascending order
    - Adds placeholder loop over sorted v (logic not yet implemented)

---
+++ Twins.cpp
@@ -0,0 +1,21 @@
+//
+// Created by hp on 23/8/2026.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    vector<int> v;
+    for (int i = 0; i < t; i++) {
+        int c;
+        cin >> c;
+        v.push_back(c);
+    }
+    sort(v.begin(), v.end());
+    int sum = 0;
+    for (int i = 0; i < v.size(); i++) {
+        
+    }
+}
