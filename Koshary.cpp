8f83a9e8bc295cdcd1e237f5175d83f6e1bf10b8
Hala M. Dalloul <haladalloul42@gmail.com>
2026-08-23 18:34:33 +0300

    Add solution for pair parity check problem
    - Reads t test cases, each with two integers x and y
    - Prints "No" if both x and y are odd
    - Prints "Yes" if at least one of them is even

---
+++ Koshary.cpp
@@ -0,0 +1,24 @@
+//
+// Created by hp on 23/8/2026.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x , y;
+        cin >> x >> y;
+        if ( x%2 != 0 && y %2 != 0) {
+            cout << "No" << endl;
+        }else {
+            cout << "Yes" << endl;
+        }
+    }
+return 0;
+}
