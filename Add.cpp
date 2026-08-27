57a2718f8dd6f42798056485efc3b5b1d1953829
Hala M. Dalloul <haladalloul42@gmail.com>
2026-08-27 23:43:25 +0300

    Simple add function

---
+++ Add.cpp
@@ -0,0 +1,15 @@
+//
+// Created by hp on 27/8/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 10e5 + 5;
+int main() {
+    int x,y;
+    if (x <= MAXN && y <= MAXN) {
+        cin>>x>>y;
+        cout<<x+y<<endl;
+    }
+
+    return 0;
+}
