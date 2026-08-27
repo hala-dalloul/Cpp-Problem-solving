c9f6aec8c9b2199f78b1b53dc6868cff464d7d4d
Hala M. Dalloul <haladalloul42@gmail.com>
2026-08-27 23:46:55 +0300

    Simple print function

---
+++ Print.cpp
@@ -0,0 +1,19 @@
+//
+// Created by hp on 27/8/2026.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 1000;
+int main() {
+    int n;
+    if (n > MAXN) {
+        cin>>n;
+        while (n--) {
+            cout<<n<<" ";
+        }
+    }
+
+
+    return 0;
+}
