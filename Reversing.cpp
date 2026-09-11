c6f98f5f1593d537696171e314189fc7f4794088
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-11 13:54:41 +0300

    Add Reversing.cpp

---
+++ Reversing.cpp
@@ -0,0 +1,13 @@
+//
+// Created by hp on 11/9/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<long long> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = n-1; i >= 0; i--) cout << arr[i] << " ";
+    return 0;
+}
