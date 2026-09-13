016b0c43515adbab9236b6d1bb83bef8ef3dac8f
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-13 23:28:25 +0300

    Add Boy or Girl.cpp

---
+++ Boy or Girl.cpp
@@ -0,0 +1,29 @@
+//
+// Created by hp on 13/9/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    cin >> s;
+    vector<bool> v(256,false);
+    int counter = 0;
+    for (int i = 0; i < s.length(); i++) {
+        char current_char = s[i];
+        cout<<current_char<<"::"<<v[current_char]<<"\n";
+        if (v[current_char] == false) {
+            v[current_char] = true;
+            counter++;
+        }
+    }
+    if (counter%2 == 0) {
+        cout << "CHAT WITH HER!";
+    }else {
+        cout<<"IGNORE HIM!";
+    }
+    return 0;
+}
