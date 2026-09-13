f672ec5dcea476addfe71e6af07c481873f8a172
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-13 09:58:51 +0300

    Add Create A New String.cpp

---
+++ Create A New String.cpp
@@ -0,0 +1,12 @@
+//
+// Created by hp on 12/9/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s,t;
+    cin >> s;
+    cin >> t;
+    cout << s.length()<<" "<<t.length()<<"\n";
+    cout<<s<<" "<<t;
+}
