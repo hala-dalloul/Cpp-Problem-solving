381be3dc52c343f1f5a0743b2584f8f47667c40d
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-13 09:57:42 +0300

    Add Easy Number Challenge.cpp

---
+++  Easy Number Challenge.cpp
@@ -0,0 +1,14 @@
+
+//
+// Created by hp on 12/9/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int n = a*b*c;
+    for (int i = 1; i <= n; i++) {
+        
+    }
+}
