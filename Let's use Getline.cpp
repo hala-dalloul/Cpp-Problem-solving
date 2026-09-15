770f2734f4af4cdfe632e3deead48c4e5873e29e
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-15 13:22:30 +0300

    Add Let's use Getline.cpp

---
+++ Let's use Getline.cpp
@@ -0,0 +1,13 @@
+//
+// Created by hp on 13/9/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string input;
+    getline(cin, input,'\\');
+    cout << input ;
+}
