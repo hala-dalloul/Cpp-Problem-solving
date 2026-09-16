be1df3fe6229a7f1ba03d63b052a52d482a173d3
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-16 21:38:05 +0300

    Update String Functions.cpp

--- String Functions.cpp
+++ String Functions.cpp
@@ -3,33 +3,54 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    int n,q;
-    cin >> n>>q;
+    int n, q;
+    cin >> n >> q;
     string s;
-    getline(cin,s);
-    if (s.length() > n) {
-        s=s.substr(0, n);
-    }
+    cin >> s;
+
     for (int i = 0; i < q; i++) {
         string x;
-        getline(cin,x);
-
-        if (x.empty()) {
-            i--;
-            continue;
+        cin >> x;
+        int e, f;
+        char p;
+        if (x == "substr") {
+            cin >> e >> f;
+            if (e > f)swap(e, f);
+            cout << s.substr((e - 1), f - e + 1) << "\n";
         }
-
-        if (x.find("substr") != string::npos) {
-            cout << s.substr(stoi(x.substr(7, 1)), stoi(x.substr(9, 1))) << "\n";
+        if (x == "sort") {
+            cin >> e >> f;
+            if (e > f)swap(e, f);
+            sort(s.begin() + e - 1, s.begin() + f);
+        }
+        if (x == "pop_back") {
+            s.pop_back();
+        }
+        if (x == "back") {
+            cout << s.back() << "\n";
+        }
+        if (x == "reverse") {
+            cin >> e >> f;
+            if (e>f)swap(e,f);
+            reverse(s.begin() + e - 1, s.begin() + f);
+        }
+        if (x == "front") {
+            cout << s.front() << "\n";
+        }
+        if (x == "push_back") {
+            cin >> p;
+            s.push_back(p);
+        }
+        if (x == "print") {
+            cin >> e;
+            cout << s[e-1] << "\n";
         }
     }
-
-
     return 0;
 }
-
