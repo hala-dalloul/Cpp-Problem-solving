6f037fa3e3b04a0f1940ccb2d15e8d56ce997c2b
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-18 17:45:51 +0300

    Add Equal Distribution.cpp

---
+++ Equal Distribution.cpp
@@ -0,0 +1,17 @@
+//
+// Created by hp on 18/9/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n,k;
+    cin >> n >> k;
+    if (n > k) cout << k;
+    else cout << 0;
+
+    return 0;
+}
