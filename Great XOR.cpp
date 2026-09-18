3e22c9a9a5fdb7fd17c9a4db7bc2b1b611df1c9c
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-18 17:46:22 +0300

    Add Great XOR.cpp

---
+++ Great XOR.cpp
@@ -0,0 +1,21 @@
+//
+// Created by hp on 18/9/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x;
+        cin >> x;
+        int counter = x/2 ;
+        cout << counter << "\n";
+    }
+
+    return 0;
+}
