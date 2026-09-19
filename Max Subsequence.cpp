cb698449a65134e767b32d8e5c42f56b9dfbaf64
Hala M. Dalloul <haladalloul42@gmail.com>
2026-09-19 22:02:25 +0300

    Add Max Subsequence.cpp

--- Max Subsequence.cpp
+++ Max Subsequence.cpp
@@ -10,37 +10,14 @@ int main() {
     cin >> n;
     string s;
     cin >> s;
-    char x = s[0];
-    for (int i = 0; i < s.length()-1; i++) {
-        if (s[s.length()-1] == s[s.length()-2]) {
-            s = s.substr(0, s.length()-1);
-        }
-        if (s[i] == s[i+1]) {
-            s= s.substr(i, s.length()-1);
-        }else {
-            cout << s.length() << endl;
-            break;
+    string ans;
+    ans.push_back(s[0]);
+    for (int i = 1; i <= s.length()-1; i++) {
+        if (s[i] != s[i-1]) {
+            ans.push_back(s[i]);
         }
     }
+    // cout << ans;
+    cout << ans.size();
     return 0;
 }
-
-
-//         if (s.length() >= 2 && s[s.length() - 1] == s[s.length() - 2]) {
-//             s = s.substr(0, s.length() - 2);
-//             // إعادة ضبط المؤشر بعد الحذف لتجنب تجاوز الحدود
-//             i = 0;
-//             continue;
-//         }
-//         // إذا كان الحرف الحالي يساوي الحرف التالي
-//         if (s[i] == s[i + 1]) {
-//             // حذف الحرفين المتشابهين عبر دالة erase
-//             s.erase(i, 2);
-//             // إرجاع المؤشر خطوة للخلف لفحص الأحرف الجديدة المتجاورة
-//             if (i > 0) i--;
-//         } else {
-//             // الانتقال للحرف التالي إذا لم يكن هناك تكرار
-//             i++;
-//         }
-//
-//     }
